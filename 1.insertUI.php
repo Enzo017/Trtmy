87e05a4121a309de4add80dec21a3675768b8531
MSI\User <roger900117@gmail.com>
2021-11-08 09:22:02 +0800

    ..

--- 1.insertUI.php
+++ 1.insertUI.php
@@ -2,31 +2,32 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>input form</title>
+<title>無標題文件</title>
 </head>
-<body>
-<p>my guest book !! </p>
-<hr />
 
-<table width="200" border="1">
-  <tr>
-    <td>title</td>
-    <td>message</td>
-    <td>name</td>
-  </tr>
-  <tr><form method="post" action="2.insert.php">
-    <td><label>
-      <input name="title" type="text" id="title" />
-    </label></td>
-    <td><label>
-      <input name="msg" type="text" id="msg" />
-    </label></td>
-    <td><label>
-      <input name="myname" type="text" id="myname" />
-      <input type="submit" name="Submit" value="送出" />
-    </label></td>
-	</form>
-  </tr>
-</table>
+<body>
+
+<p>insert new message</p>
+<hr />
+<?php
+require('dbconfig.php');
+
+$title=$_POST['title'];
+$msg=$_POST['msg'];
+$name=$_POST['myname'];
+
+if ($title) {
+	$sql = "insert into guestbook (title, msg, name) values (?, ?, ?)";
+	$stmt = mysqli_prepare($db, $sql); //prepare sql statement
+	mysqli_stmt_bind_param($stmt, "sss", $title, $msg,$name); //bind parameters with variables
+	mysqli_stmt_execute($stmt);  //執行SQL
+	echo "message added.";
+} else {
+	echo "empty title, cannot insert.";
+}
+?>
+<hr>
+<a href="1.listUI.php">Home</a>
+
 </body>
 </html>
